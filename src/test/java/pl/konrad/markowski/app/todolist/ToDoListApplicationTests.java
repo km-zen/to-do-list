60ccc9646102b7dab16da556bc00007a3691ec02
Konrad Markowski <konrad.markowski.1993@gmail.com>
2022-08-23 17:36:16 +0200

    basic class created

---
+++ src/test/java/pl/konrad/markowski/app/todolist/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.konrad.markowski.app.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
