13bc6c80f7718a6c10c77a4642d8d5a662a1aa7e
Konrad Markowski <konrad.markowski.1993@gmail.com>
2022-08-23 17:34:36 +0200

    basic class created

---
+++ src/main/java/pl/konrad/markowski/app/todolist/web/model/UserModel.java
@@ -0,0 +1,40 @@
+package pl.konrad.markowski.app.todolist.web.model;
+
+public class UserModel {
+    private String login;
+    private String email;
+    private String password;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "UserModel{" +
+                "login='" + login + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
